3021514fa28f7ce024bd1a54cf7811599fb5ef65
Niederbach <a.niederbach@outlook.com>
2024-12-04 10:03:23 +0100

    Add project files.

---
+++ AnimalHospital/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHospital
+{
+    public class Animal
+    {
+        public string AnimalType { get; set; }
+        public string AnimalName { get; set; }
+        public int AnimalAge { get; set; }
+        public bool IsVaccinated { get; set; }
+        public int AmountOfLegs { get; set; }
+        public List<Animal> GetAnimals()
+        {
+            var animals = new List<Animal>();
+
+            animals.Add(new Animal { AnimalName = "Stefan", AnimalType = "Katt", AnimalAge = 10, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Alice", AnimalType = "Hund", AnimalAge = 5, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Max", AnimalType = "Hund", AnimalAge = 6, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Olivia", AnimalType = "Kanin", AnimalAge = 2, IsVaccinated = false, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Liam", AnimalType = "Katt", AnimalAge = 12, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Ella", AnimalType = "Skölpadda", AnimalAge = 20, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Sofia", AnimalType = "Marsvin", AnimalAge = 1, IsVaccinated = false, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Alice", AnimalType = "Hund", AnimalAge = 3, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Max", AnimalType = "Katt", AnimalAge = 5, IsVaccinated = true, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Stefan", AnimalType = "Marsvin", AnimalAge = 2, IsVaccinated = false, AmountOfLegs = 4 });
+            animals.Add(new Animal { AnimalName = "Emma", AnimalType = "Katt", AnimalAge = 10, IsVaccinated = true, AmountOfLegs = 4 });
+           
+            return animals;
+        }
+    }
+}
---
+++ AnimalHospital/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace AnimalHospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new Animal();
+            var ListOfAnimals = animals.GetAnimals().ToList();
+            
+            //=================================================================
+            //A.
+            var amountOfDog = ListOfAnimals.Count(x => x.AnimalType == "Hund");
+
+            Console.WriteLine($"Antal hundar: {amountOfDog}");
+            Console.WriteLine();
+
+            //=================================================================
+            //B.
+            var oldestAnimal = ListOfAnimals.Max(x => x.AnimalAge);
+
+            Console.WriteLine($"Äldsta djuret: {oldestAnimal}");
+            Console.WriteLine();
+
+            //=================================================================
+            //C.
+            var IsVaccinated = ListOfAnimals.Where(x => x.IsVaccinated == true).ToList();
+
+            Console.WriteLine("Dem Här djuren är vaccinerade");
+            Console.WriteLine();
+
+            foreach (var animal in IsVaccinated)
+            {
+                Console.WriteLine($"Namn: {animal.AnimalName}");
+                Console.WriteLine($"Typ: {animal.AnimalType}");
+                Console.WriteLine($"Vaccinarad: {animal.IsVaccinated}");
+                Console.WriteLine("-----------------");
+
+            }
+
+            //=================================================================
+            //D.
+            var filterAnimals = ListOfAnimals
+                .Where(x => x.AmountOfLegs == 4)
+                .Where(x => x.AnimalAge > 3)
+                .ToList();
+
+            Console.WriteLine("Dem Här djuren är Filtrerade");
+            Console.WriteLine();
+
+            foreach (var animal in filterAnimals)
+            {
+                Console.WriteLine($"Namn: {animal.AnimalName}");
+                Console.WriteLine($"Ålder: {animal.AnimalAge}");
+                Console.WriteLine($"Antal ben: {animal.AmountOfLegs}");
+                Console.WriteLine("-----------------");
+
+            }
+
+            //=================================================================
+            //E.
+
+
+
+
+
+
+
+        }
+    }
+}
